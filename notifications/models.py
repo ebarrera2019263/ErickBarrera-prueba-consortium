8dbce26a7983c4238003c6efbc3ba9f92b0ffbda
ebarrera2019263 <ebarrera-2019263@kinal.edu.gt>
2025-04-04 06:01:48 -0600

    se defie el modelo de usuario que recibe la notificacion , se crearon las migraciones en base al modelo

--- notifications/models.py
+++ notifications/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# aqui se define el modelo de la notificación
+
+
+
+class Notification(models.Model):
+    RECEPCIONISTAS = [
+        ('amanda', 'Amanda González'),
+        ('wanda', 'Wanda Pastor'),
+    ]
+
+    #agregar el campo de usuario que recibe la notificación
+
+    fecha_recepcion = models.DateField()
+    hora_recepcion = models.TimeField()
+    entidad_emisora = models.CharField(max_length=255)
+    numero_expediente = models.CharField(max_length=100)
+    dirigido_a = models.CharField(max_length=255)
+    recepcionista = models.CharField(max_length=10, choices=RECEPCIONISTAS)
+    
+    entregado_a = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    fecha_entrega = models.DateField(null=True, blank=True)
+    hora_entrega = models.TimeField(null=True, blank=True)
+
+    
