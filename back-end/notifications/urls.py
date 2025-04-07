849e16a14d9006741af6b92453d6d51f53e1aa5e
ebarrera2019263 <ebarrera-2019263@kinal.edu.gt>
2025-04-06 21:34:36 -0600

    se cargo configs para subirlo a la nube v4

--- back-end/notifications/urls.py
+++ back-end/notifications/urls.py
@@ -1,8 +1,21 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from django.contrib.auth.models import User
 
 from .views import NotificationViewSet, UsuarioViewSet
 
+# 
+def create_admin():
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser(
+            username='admin',
+            email='admin@example.com',
+            password='admin123'
+        )
+
+create_admin()
+
+# 📦 Rutas
 router = DefaultRouter()
 router.register(r'notificaciones', NotificationViewSet, basename='notificacion')
 router.register(r'usuarios', UsuarioViewSet, basename='usuario') 
